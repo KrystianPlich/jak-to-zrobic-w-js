0610e7762c3bb4e4a162052db1010b8587f82ab8
Krystian Plich <KrystianPlich@github.com>
2021-08-10 16:44:15 +0200

    Task2 - Create page object assertions

--- excercsises/2_page_object/contact.spec.js
+++ excercsises/2_page_object/contact.spec.js
@@ -1,11 +1,14 @@
-//var homePage = require('...');
+var contactPage = require('./pages/contact');
 
 describe('Protractor Workshop app', function() {
 
 	beforeEach(function () {
+		contactPage.get()
 	});
 
 	it('should have "Get in touch" and "Address" sections on Contact Us page', function(){
+		expect(contactPage.getInTouch().getText()).toEqual("Get in Touch");
+		expect(contactPage.getAdress().getText()).toEqual("Address");
 	});
 
 
