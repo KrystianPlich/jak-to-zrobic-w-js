d89fc9d5c3e1f65f6dbceac143d3bf34791c0b62
Krystian Plich <KrystianPlich@github.com>
2021-08-11 13:13:53 +0200

    Task3 - Updated assertions

--- excercsises/3_synchronization/contact.spec.js
+++ excercsises/3_synchronization/contact.spec.js
@@ -7,12 +7,11 @@ describe('Protractor Workshop app', function() {
 	});
 
 	it('hould have Contact page with title "Protractor workshop | Contact us"', function(){
-		expect(contactPage.getTitle()).toEqual("Protractor workshop | Home");
+		expect(contactPage.getTitle()).toEqual("Protractor workshop | Contact us");
 	});
 
-	xit('should display text "Your message has been sent." when user sends message  ', function(){
-		
-
+	it('should display text "Your message has been sent." when user sends message  ', function(){
+		contactPage.sendMessage('Test', 'sample@email.com', 'Content');
+		expect(contactPage.getMessageSent().getText()).toEqual("Your message has been sent.")
 	});
-
 });
